c17a9f47800b7dd02e0e66932a45991632661c40
kazu.n <lumi.github@gmail.com>
2018-04-30 19:35:01 +0900

    for heroku

---
+++ app/views/events/config/environments/development.rb
@@ -0,0 +1,91 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp/caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # メールの送信
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  	address:               'smtp.gmail.com',
+  	port:      	           587,
+  	user_name:             'post.connect.holiday@gmail.com',
+  	password:              'nsemaster392',
+  	authentication:        'plain',
+  	enable_starttls_auto:  true
+  }
+
+  # S3
+  config.paperclip_defaults = {
+    :storage        => 's3',
+    :bucket         => 'connect-holiday-development',
+    :s3_region   => 'ap-northeast-1',
+    :s3_permissions => 'public-read',
+    :s3_host_name => 's3-ap-northeast-1.amazonaws.com',
+
+    :s3_credentials => {
+      access_key_id: 'AKIAIRZDHNAR5WMQMJTA',
+      secret_access_key: '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'
+    }
+  }
+
+  # S3
+  #config.paperclip_defaults = {
+  #:storage        => :s3,
+  #:bucket         => ENV['S3_BUCKET_NAME'],
+  #:s3_region      => ENV['AWS_REGION'],
+  #:s3_host_name   => 's3-ap-northeast-1.amazonaws.com',
+  #:s3_credentials => {
+  #  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  #  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+  #  }
+  #}
+end
